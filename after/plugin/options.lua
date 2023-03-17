98e0d65ca79d412bcbba9bc6da40f764f08551b1
notwhale <notwhale@gmail.com>
2023-03-17 14:34:38 +0500

    minir changes in options

--- after/plugin/options.lua
+++ after/plugin/options.lua
@@ -1,53 +1,49 @@
--- -- options
--- vim.opt.backspace = '2'
--- vim.opt.showcmd = true
--- vim.opt.laststatus = 2
--- vim.opt.autowrite = true
--- vim.opt.cursorline = true
--- vim.opt.autoread = true
---
--- vim.opt.tabstop = 2
--- vim.opt.shiftwidth = 2
--- vim.opt.shiftround = true
--- vim.opt.expandtab = true
---
--- vim.opt.clipboard = "unnamedplus"
---
--- -- Visual
--- vim.o.conceallevel       = 0                            -- Don't hide quotes in markdown
--- vim.o.cmdheight          = 1
--- vim.o.pumheight          = 10
--- vim.o.showmode           = false
--- vim.o.showtabline        = 2                            -- Always show tabline
--- vim.o.title              = true
--- vim.o.termguicolors      = true                         -- Use true colors, required for some plugins
--- vim.wo.number            = true
--- vim.wo.relativenumber    = true
--- vim.wo.signcolumn        = 'yes'
--- vim.wo.cursorline        = true
---
--- -- Behaviour
--- vim.o.hlsearch           = false
--- vim.o.ignorecase         = true                         -- Ignore case when using lowercase in search
--- vim.o.smartcase          = true                         -- But don't ignore it when using upper case
--- vim.o.smarttab           = true
--- vim.o.smartindent        = true
--- vim.o.expandtab          = true                         -- Convert tabs to spaces.
--- vim.o.tabstop            = 2
--- vim.o.softtabstop        = 2
--- vim.o.shiftwidth         = 2
--- vim.o.splitbelow         = true
--- vim.o.splitright         = true
--- vim.o.scrolloff          = 12                           -- Minimum offset in lines to screen borders
--- vim.o.sidescrolloff      = 8
--- vim.o.mouse              = 'a'
---
--- -- Vim specific
--- vim.o.hidden             = true                         -- Do not save when switching buffers
--- vim.o.fileencoding       = "utf-8"
--- vim.o.spell              = false
--- vim.o.spelllang          = "en_us"
--- vim.o.completeopt        = "menuone,noinsert,noselect"
--- vim.o.wildmode           = "longest,full"               -- Display auto-complete in Command Mode
--- vim.o.updatetime         = 300                          -- Delay until write to Swap and HoldCommand event
--- vim.g.do_file_type_lua   = 1
+-- options
+vim.o.backspace        = '2'
+vim.o.showcmd          = true
+vim.o.laststatus       = 2
+vim.o.autowrite        = true
+vim.o.autoread         = true
+
+-- Clipboard
+vim.o.clipboard        = "unnamedplus"
+
+-- Visual
+vim.o.conceallevel     = 0 -- Don't hide quotes in markdown
+vim.o.cmdheight        = 1
+vim.o.pumheight        = 10
+vim.o.showmode         = false
+-- vim.o.showtabline      = 2      -- Always show tabline
+-- vim.o.title            = true
+vim.o.termguicolors    = true -- Use true colors, required for some plugins
+vim.wo.number          = true
+vim.wo.relativenumber  = true
+vim.wo.signcolumn      = 'yes'
+vim.wo.cursorline      = true
+
+-- Behaviour
+vim.o.hlsearch         = false
+vim.o.ignorecase       = true -- Ignore case when using lowercase in search
+vim.o.smartcase        = true -- But don't ignore it when using upper case
+vim.o.smarttab         = true
+vim.o.smartindent      = true
+vim.o.expandtab        = true -- Convert tabs to spaces.
+vim.o.tabstop          = 2
+vim.o.softtabstop      = 2
+vim.o.shiftwidth       = 2
+vim.o.splitbelow       = true
+vim.o.splitright       = true
+-- vim.o.scrolloff        = 12 -- Minimum offset in lines to screen borders
+-- vim.o.sidescrolloff    = 8
+vim.o.mouse            = 'a'
+vim.o.shiftround       = true
+
+-- Vim specific
+vim.o.hidden           = true -- Do not save when switching buffers
+vim.o.fileencoding     = "utf-8"
+vim.o.spell            = false
+vim.o.spelllang        = "en_us"
+vim.o.completeopt      = "menuone,noinsert,noselect"
+vim.o.wildmode         = "longest,full" -- Display auto-complete in Command Mode
+vim.o.updatetime       = 300            -- Delay until write to Swap and HoldCommand event
+vim.g.do_file_type_lua = 1
