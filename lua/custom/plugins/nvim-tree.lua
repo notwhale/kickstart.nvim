32b6fc9cc807d1dd0fdedc150f470a5fbf3c7152
notwhale <notwhale@gmail.com>
2023-03-17 14:35:36 +0500

    setting up nvim-tree

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -9,15 +9,35 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  config = function ()
-    require('neo-tree').setup ({
+  config = function()
+    require('neo-tree').setup({
       filesystem = {
         filtered_items = {
-          hide_dotfiles = true,
-          hide_gitignored = true,
-        }
+          visible = true, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false, -- only works on Windows for hidden files/directories
+          hide_by_name = {
+            ".DS_Store",
+            "thumbs.db",
+            --"node_modules",
+          },
+          hide_by_pattern = {
+            --"*.meta",
+            --"*/src/*/tsconfig.json",
+          },
+          always_show = { -- remains visible even if other settings would normally hide it
+            ".gitignored",
+          },
+          never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+            --".DS_Store",
+            --"thumbs.db",
+          },
+          never_show_by_pattern = { -- uses glob style patterns
+            --".null-ls_*",
+          },
+        },
       }
     })
   end,
 }
-
