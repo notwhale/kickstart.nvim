b03e11bab650a15eef8104a8df293013dd69a154
notwhale <notwhale@gmail.com>
2023-03-23 08:44:34 +0500

    wip

--- after/plugin/filetype.lua
+++ after/plugin/filetype.lua
@@ -2,8 +2,6 @@ vim.filetype.add({
   pattern = {
     ['.*inventory*'] = 'ini',
     ['.*hosts*'] = 'ini',
-    -- ['.*play*.{yml,yaml}'] = 'yaml.ansible',
-    ['.{yml,yaml}'] = 'yaml.ansible',
-    ['roles/tasks/*.{yml,yaml}'] = 'yaml.ansible'
+    ['.*playbook.*'] = 'yaml.ansible',
   },
 })
