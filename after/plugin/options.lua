10be229e532ac537d5a335106a51969655cee22c
notwhale <notwhale@gmail.com>
2023-04-11 14:02:33 +0500

    refactor config

--- after/plugin/options.lua
+++ after/plugin/options.lua
@@ -1,49 +1,59 @@
--- options
-vim.o.backspace        = '2'
-vim.o.showcmd          = true
-vim.o.laststatus       = 2
-vim.o.autowrite        = true
-vim.o.autoread         = true
+-- -- options
+-- vim.o.backspace        = '2'
+-- vim.o.showcmd          = true
+-- vim.o.laststatus       = 2
+-- vim.o.autowrite        = true
+-- vim.o.autoread         = true
 
 -- Clipboard
-vim.o.clipboard        = "unnamedplus"
+vim.o.clipboard       = "unnamedplus"
 
 -- Visual
-vim.o.conceallevel     = 0 -- Don't hide quotes in markdown
-vim.o.cmdheight        = 1
-vim.o.pumheight        = 10
-vim.o.showmode         = false
--- vim.o.showtabline      = 2      -- Always show tabline
--- vim.o.title            = true
-vim.o.termguicolors    = true -- Use true colors, required for some plugins
-vim.wo.number          = true
-vim.wo.relativenumber  = true
-vim.wo.signcolumn      = 'yes'
-vim.wo.cursorline      = true
+-- vim.o.conceallevel     = 0 -- Don't hide quotes in markdown
+-- vim.o.cmdheight        = 1
+-- vim.o.pumheight        = 10
+-- vim.o.showmode         = false
+-- -- vim.o.showtabline      = 2      -- Always show tabline
+-- -- vim.o.title            = true
+vim.o.termguicolors   = true -- Use true colors, required for some plugins
+vim.wo.number         = true
+vim.wo.relativenumber = true
+vim.wo.signcolumn     = 'yes'
+vim.wo.cursorline     = true
+
+vim.o.textwidth       = 80
+vim.wo.colorcolumn    = '+1'
+vim.wo.wrap           = true
+vim.o.foldcolumn      = 'auto'
 
 -- Behaviour
-vim.o.hlsearch         = false
-vim.o.ignorecase       = true -- Ignore case when using lowercase in search
-vim.o.smartcase        = true -- But don't ignore it when using upper case
-vim.o.smarttab         = true
-vim.o.smartindent      = true
-vim.o.expandtab        = true -- Convert tabs to spaces.
-vim.o.tabstop          = 2
-vim.o.softtabstop      = 2
-vim.o.shiftwidth       = 2
-vim.o.splitbelow       = true
-vim.o.splitright       = true
+vim.o.hlsearch        = false
+vim.o.ignorecase      = true -- Ignore case when using lowercase in search
+vim.o.smartcase       = true -- But don't ignore it when using upper case
+-- vim.o.smarttab        = true
+-- vim.o.smartindent     = true
+vim.o.breakindent     = true
+vim.o.autoindent      = true
+vim.o.expandtab       = true -- Convert tabs to spaces.
+vim.o.tabstop         = 2
+vim.o.softtabstop     = 2
+vim.o.shiftwidth      = 2
+vim.o.splitbelow      = true
+vim.o.splitright      = true
 -- vim.o.scrolloff        = 12 -- Minimum offset in lines to screen borders
 -- vim.o.sidescrolloff    = 8
-vim.o.mouse            = 'a'
-vim.o.shiftround       = true
+vim.o.mouse           = 'a'
+vim.o.shiftround      = true
+vim.o.undofile        = true
 
 -- Vim specific
-vim.o.hidden           = true -- Do not save when switching buffers
-vim.o.fileencoding     = "utf-8"
-vim.o.spell            = false
-vim.o.spelllang        = "en_us"
-vim.o.completeopt      = "menuone,noinsert,noselect"
-vim.o.wildmode         = "longest,full" -- Display auto-complete in Command Mode
-vim.o.updatetime       = 300            -- Delay until write to Swap and HoldCommand event
-vim.g.do_file_type_lua = 1
+-- vim.o.hidden           = true -- Do not save when switching buffers
+vim.o.fileencoding    = "utf-8"
+-- vim.o.spell            = false
+-- vim.o.spelllang        = "en_us"
+vim.o.completeopt     = "menuone,noinsert,noselect"
+-- vim.o.wildmode         = "longest,full" -- Display auto-complete in Command Mode
+vim.o.updatetime      = 300 -- Delay until write to Swap and HoldCommand event
+vim.o.timeout         = true
+vim.o.timeoutlen      = 300
+-- vim.g.do_file_type_lua = 1
--- init.lua
+++ init.lua
@@ -64,7 +64,7 @@ vim.opt.rtp:prepend(lazypath)
 --    as they will be available in your neovim runtime.
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
-  'b0o/schemastore.nvim',
+  -- 'b0o/schemastore.nvim',
 
   -- Git related plugins
   'tpope/vim-fugitive',
@@ -476,40 +476,7 @@ local servers = {
   -- taplo = {},
   terraformls = {},
   -- lemminx = {},
-  yamlls = {
-    yaml = {
-      schemaStore = {
-        enable = true,
-        -- url = "https://www.schemastore.org/json",
-      },
-      -- schemas = {
-      -- kubernetes = "*.yaml",
-      -- ["http://json.schemastore.org/composer"] = "/*",
-      --   ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/inventory.json"] = "{*inventory*,*hosts*}",
-      --   ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/tasks"] = "roles/tasks/*.{yml,yaml}",
-      -- ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/playbook"] = "site.yml",
-      -- ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-      -- ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-      -- ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = "azure-pipelines.yml",
-      -- ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-      -- ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-      -- ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-      -- ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-      -- ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
-      -- ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-      -- ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-      -- ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
-      -- },
-      format = {
-        enable = true,
-        singleQuote = true,
-        printWidth = 120,
-      },
-      validate = true,
-      completion = true,
-      hover = true,
-    },
-  },
+  yamlls = {},
 }
 
 local tools = {
@@ -559,6 +526,43 @@ mason_lspconfig.setup_handlers {
       settings = servers[server_name],
     }
   end,
+}
+
+require('lspconfig')['yamlls'].setup {
+  settings = {
+    yaml = {
+      schemaStore = {
+        enable = true,
+        -- url = "https://www.schemastore.org/json",
+      },
+      -- -- schemas = {
+      -- kubernetes = "*.yaml",
+      -- ["http://json.schemastore.org/composer"] = "/*",
+      --   ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/inventory.json"] = "{*inventory*,*hosts*}",
+      --   ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/tasks"] = "roles/tasks/*.{yml,yaml}",
+      -- ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/playbook"] = "site.yml",
+      -- ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+      -- ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+      -- ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = "azure-pipelines.yml",
+      -- ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+      -- ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+      -- ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+      -- ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+      -- ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
+      -- ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+      -- ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+      -- ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+      -- },
+      format = {
+        enable = true,
+        singleQuote = true,
+        printWidth = 120,
+      },
+      validate = true,
+      completion = true,
+      hover = true,
+    },
+  },
 }
 
 -- nvim-cmp setup
